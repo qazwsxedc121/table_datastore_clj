2bfae17772f7969fc293ff46bb172fa6c54e4b78
guoxc <qazwsxedc121@gmail.com>
2014-05-07 11:29:37 +0800

    add doc & as-map functions

--- README.md
+++ README.md
@@ -6,11 +6,31 @@ A "Table" is a vector of items with same sechema.
 
 ## Usage
 
+### new table
 
+```
+(def table-example ["name" "sex" "age"] [["john" "M" 13] ["marry" "F" 13]])
+```
+
+### find using key and value
+
+```
+(find-one-k-v table-example "age" 13)
+=> ["john" "M" 13]
+
+(find-one-k-v-as-map table-example "age" 13)
+=> {"name" "john", "sex" "M", "age" 13}
+
+(find-all-k-v table-example "age" 13)
+=> (["john" "M" 13] ["marry" "F" 13])
+
+(find-all-k-v-as-map table-example "age" 13)
+=> ({"name" "john", "sex" "M", "age" 13} {"name" "marry", "sex" "F", "age" 13})
+```
 
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2014 qazwsxedc121@gmail.com
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- src/table_datastore_clj/core.clj
+++ src/table_datastore_clj/core.clj
@@ -5,7 +5,12 @@
   ([schema initdata]
    {:schema schema :data initdata}))
 
+;;(def table-example ["name" "sex" "age"] [["john" "M" 13] ["marry" "F" 13]])
+
 (defn find-one-k-v [t k v]
+  "find one entry in table , params(t=table k=key v=value), return a array"
+;;(find-one-k-v table-example "age" 13)
+;;=> ["john" "M" 13]
   (let [key-index (.indexOf (:schema t) k)]
     (loop [r (:data t)]
       (cond
@@ -13,13 +18,33 @@
        (= (nth (first r) key-index) v) (first r)
        :else (recur (rest r))))))
 
+(defn find-one-k-v-as-map [t k v]
+  "find one entry as a map in table"
+;;(find-one-k-v-as-map table-example "age" 13)
+;;=> {"name" "john", "sex" "M", "age" 13}
+  (to-map t (find-one-k-v t k v)))
+
 (defn find-all-k-v [t k v]
+  "find all entry in table , params(t=table k=key v=value), return a list of array"
+;;(find-all-k-v table-example "age" 13)
+;;=> (["john" "M" 13] ["marry" "F" 13])
   (let [key-index (.indexOf (:schema t) k)]
     (filter #(= (nth % key-index) v) (:data t))))
+
+(defn find-all-k-v-as-map [t k v]
+  "find all entry as map in table"
+;;(find-all-k-v-as-map table-example "age" 13)
+;;=> ({"name" "john", "sex" "M", "age" 13} {"name" "marry", "sex" "F", "age" 13})
+  (map #(to-map t %)
+       (find-all-k-v t k v)))
 
 (defn columns [t k]
   (let [key-index (.indexOf (:schema t) k)]
     (map #(nth % key-index) (:data t))))
+
+(defn to-map [t i]
+  (let [schema (:schema t)]
+    (zipmap schema i)))
 
 (defn to-maps [t]
   (let [schema (:schema t)
